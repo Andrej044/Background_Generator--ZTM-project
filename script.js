55295f5eac6f8f4ff3c16497503ec2c972d25058
Andrii Lishchyna <andrej044@i.ua>
2023-05-23 13:52:01 -0400

    add rgb  to h3

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let css = document.querySelector("h3");
+let hexCss = document.querySelector(".hexCss");
+let rgbaCss = document.querySelector(".rgbCss");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.querySelector("body");
@@ -6,7 +7,8 @@ let body = document.querySelector("body");
 
 function getInputValue(){
   body.style.background = `linear-gradient(to right, ${color1.value} , ${color2.value})`;
-  css.textContent = `linear-gradient(to right, ${color1.value} , ${color2.value})`;
+  hexCss.textContent = `linear-gradient(to right, ${color1.value} , ${color2.value})`;
+  rgbaCss.textContent = body.style.background
 }
 
 color1.addEventListener("input", getInputValue);
